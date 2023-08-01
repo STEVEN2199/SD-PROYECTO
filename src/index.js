bd59947a2e2cf515fac092e12871dc264421914c
edumbrav <edumbrav@espol.edu.ec>
2023-08-01 17:54:03 -0500

    no vale

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const app = require('./app');
 
-app.listen( app.get('port') , (req ,res) => {
-    console.log('Servidor corriendo en el puerto' , app.get('port'));
+app.listen(app.get('port'), (req, res) => {
+    console.log('Servidor corriendo en el puerto', app.get('port'));
+});
+
+// Cerrar el cliente de Redis después de detener el servidor
+app.on('close', () => {
+    client.quit();
 });
